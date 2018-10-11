9eff63737385737922bc13ee5ed6504ac42fd428
Alan <a386226@gmail.com>
2018-10-11 16:31:56 -0700

    not sure which ajax request to use, search or recommended

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,29 +1,53 @@
-
-let accessToken = 'BQD5EJonSMdC8uf104SMJFB9yqrKI2_Li3YcGb6kdQ7qpA5Sqd6o9c9jeEsJazd-JRBE-U5tKtgBe_y7iv5PNnLaO4jKuUbN4fO-ndJCLiCoPr8-IiuDklmQN8mlpIBTaGHHASaFf1WOObTv5VGtxqT8qvEYaH4M50gHSw&refresh_token=AQATV6p5ed6IHxS3M6wat6I_H-5I_JAaD0cE9TDv0n38q9YWsQ7-qvzt79wsGvNCs0uBUxXvhs5hoZvqFPTncnoOvDp7fEg_T4MYf31B0sNInf28h495qkP2HLeaUQVtLugj9g';
-// let clientId = '74893e3303c047d68148a47c4ef102bd'
+let accessToken = 'BQDGzOPTEKX2lDZiKq6Gfdyh-r_co7IPtE5oKSRPPVVjTNbJ-wF8_Lz1yGVk0kVYnEjoU4_nRNwPOjrrcTU-r7nhd8wPHZGeXT8fldcx1eX2s2aCMCSmGxAvZOJpll9J0zFEYQnX2scp7z1ZA9dmC2GPM1wCb6c52E-8IQ';
 
 let artistSuccess = (res) => {
     let artistId = res.artists.items[0].id;
-    // console.log(artistId);
+    console.log(artistId);
+    //ajax request for related artists
     $.ajax({
         method: 'GET',
-        url: `https://api.spotify.com/v1/recommendations?seed_artists=${artistId}`,
+        url: `https://api.spotify.com/v1/artists/${artistId}/related-artists`,
         headers: {
             'Authorization': 'Bearer ' + accessToken
         }, 
-        success: rec = (res) => {
-            // console.log(res.tracks)
+        success: recommend = (res) => {
+            console.log(res)
             let i;
-            for (i=0; i < res.tracks.length; i++) {
-                let recommendations = res.tracks[i].artists[0].name
+            for (i=0; i < res.artists.length; i++) {
+                let newArtists = res.artists[i].name
+                let popularity = res.artists[i].popularity
+                let url = res.artists[i].external_urls.spotify
                 let artistList = 
                 `<li>
-                <h2> ${recommendations} </h2>
+                <p><a href="${url}">${newArtists}</a> ${popularity} </p>
+                
                 </li>`
+
                 $('#testDiv').append(artistList);
             }    
         }
     })
+    // ajax request for recommended tracks
+    // $.ajax({
+    //     method: 'GET',
+    //     url: `https://api.spotify.com/v1/recommendations?seed_artists=${artistId}`,
+    //     headers: {
+    //         'Authorization': 'Bearer ' + accessToken
+    //     }, 
+    //     success: recommend = (res) => {
+    //         console.log(res.tracks)
+    //         let i;
+    //         for (i=0; i < res.tracks.length; i++) {
+    //             let recommendations = res.tracks[i].artists[0].name
+    //             let artistList = 
+    //             `<li>
+    //             <p> ${recommendations} </p>
+    //             </li>`
+
+    //             $('#testDiv').append(artistList);
+    //         }    
+    //     }
+    // })
 }
 
 $('.fas').on('click', (e) => {
@@ -38,8 +62,6 @@ $('.fas').on('click', (e) => {
         success: artistSuccess,
         // error: console.log('error')
     });
-
-
 })
 
 $('#signUp').on('click', (e) => {
@@ -56,5 +78,3 @@ $('#about').on('click', (e) => {
     e.preventDefault();
     console.log('about')
 })
-
-

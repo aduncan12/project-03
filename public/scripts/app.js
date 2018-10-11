063c5d612c1590619ce9efe9b9ccae04febf63a7
Alan <a386226@gmail.com>
2018-10-11 15:02:21 -0700

    ajax request done, need to filter and style results

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,24 +1,27 @@
 
-let accessToken = 'BQDe7F4-wr3pXLqK-3umdX5Bw9Z26jeCOUPSPxkdtCi2PiH1H3rAv3xrwlbnq57fTh9we_t18ozr8W0ss6C3ug_pRO-6TjS8fhfInliPmz0mB_0wbdVqNEhZAOsXVufehrHeKV7_lfmBNfhelTG7Y1tRgDxUoVEHrbbwGQ&refresh_token=AQCHZYqYUHHRHbD7ijVedRQBXNJgdjHKooi7V0LZfzkSa3sfp_oWD-rnKpcyqhr5ke4SXYwlh2x1LVhKWmEVIOgjflck8qa3W8mbxh8gUaEL9XYdVTvyB_qcVI5lTkDDgFpEPg';
+let accessToken = 'BQD5EJonSMdC8uf104SMJFB9yqrKI2_Li3YcGb6kdQ7qpA5Sqd6o9c9jeEsJazd-JRBE-U5tKtgBe_y7iv5PNnLaO4jKuUbN4fO-ndJCLiCoPr8-IiuDklmQN8mlpIBTaGHHASaFf1WOObTv5VGtxqT8qvEYaH4M50gHSw&refresh_token=AQATV6p5ed6IHxS3M6wat6I_H-5I_JAaD0cE9TDv0n38q9YWsQ7-qvzt79wsGvNCs0uBUxXvhs5hoZvqFPTncnoOvDp7fEg_T4MYf31B0sNInf28h495qkP2HLeaUQVtLugj9g';
 // let clientId = '74893e3303c047d68148a47c4ef102bd'
 
 let artistSuccess = (res) => {
     let artistId = res.artists.items[0].id;
-    console.log(artistId);
-    // let artistList = `<p>${res.artists.items[0].name} ${artistId} </p>`
-    // $('#testDiv').append(artistList);  
+    // console.log(artistId);
     $.ajax({
         method: 'GET',
         url: `https://api.spotify.com/v1/recommendations?seed_artists=${artistId}`,
         headers: {
             'Authorization': 'Bearer ' + accessToken
         }, 
-        success: recommendations = (res) => {
-            console.log(res.tracks)
+        success: rec = (res) => {
+            // console.log(res.tracks)
             let i;
             for (i=0; i < res.tracks.length; i++) {
-                console.log(res.tracks[i].artists[0].name)
-            }
+                let recommendations = res.tracks[i].artists[0].name
+                let artistList = 
+                `<li>
+                <h2> ${recommendations} </h2>
+                </li>`
+                $('#testDiv').append(artistList);
+            }    
         }
     })
 }
